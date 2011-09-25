51f102a29930b47bd250bd5a6a81412f26e6147d
pussbb <pussbb@gmail.com>
2011-09-25 13:38:40 +0300

    check book

--- classes/controller/rpc.php
+++ classes/controller/rpc.php
@@ -12,6 +12,12 @@ class Controller_RPC extends Controller {
                               array( $this->_rpc->xmlrpcInt, $this->_rpc->xmlrpcInt, $this->_rpc->xmlrpcInt)  ,
                               array( $this->_rpc->xmlrpcString)
                             )
+                          ),
+                          "qhda.bookcheck" => array(
+                            "function" => array($this,'bookcheck'),
+                            "signature" => array(
+                              array( $this->_rpc->xmlrpcInt, $this->_rpc->xmlrpcStruct)
+                            )
                           )
                         );
         $this->_rpc->xmlRPCServer($methods);
@@ -23,5 +29,62 @@ class Controller_RPC extends Controller {
 
        $count = $params[0] + $params[1];
        return new xmlrpcresp(new xmlrpcval($count, "int"));
+    }
+
+    public function bookcheck($m)
+    {
+       $params = php_xmlrpc_decode($m);
+       $data = $params[0];
+
+       if(empty($data['userName']) || empty($data['apiKey']))
+           return new xmlrpcresp(0, 301,__('Username or apikey are empty'));
+
+       $member = ORM::factory('user')->where('username','=',$data['userName'])
+                                       ->and_having('apikey','=',$data['apiKey'])
+                                         ->find();
+
+       if($member->has('roles',  ORM::factory('role', array('name' => 'login'))) == FALSE)
+           return new xmlrpcresp(0, 302,__('You are not allowed to add new books'));
+
+       $book = DB::select()->from('books')->where('name','=',$data['bookName'])
+                                            ->join('book_access')
+                                              ->on('book_access.book_id','=','books.id')
+                                               ->execute();
+
+        if(count($book) == 0)
+             return $this->_addBook($data,$member->id);
+
+       if(!in_array($member->id,Arr::pluck($book,'userid')))
+                return new xmlrpcresp(0, 302,__('You are not allowed to change somthing in this books.'));
+
+       $book_id = Arr::pluck($book,'id');
+       return new xmlrpcresp(new xmlrpcval( $book_id[0],'int'));
+
+    }
+
+    private function _addBook($data,$user_id)
+    {
+         try
+         {
+             list($insert_id, $total_rows) = DB::insert('books', array('name', 'description'))
+                     ->values(array($data['bookName'], $data['bookDescription']))
+                     ->execute();
+
+             DB::insert('book_access', array('book_id', 'userid'))
+                     ->values(array($insert_id, $user_id))
+                     ->execute();
+             if (!empty($data['image'])) {
+                 $data_img = base64_decode($data['image']);
+                 $im = imagecreatefromstring($data_img);
+                 imagesavealpha($im, TRUE);
+                 imagepng($im, DOCROOT . 'public/' . $data['bookImage'] . '.png', 5, PNG_ALL_FILTERS);
+                 imagedestroy($im);
+             }
+             return new xmlrpcresp(new xmlrpcval($insert_id, 'int'));
+         }
+         catch (Database_Exception $e)
+         {
+             return new xmlrpcresp(0, 7802, $e->getMessage());
+         }
     }
 }
--- config/rpc.php
+++ config/rpc.php
@@ -5,7 +5,7 @@
 
 return array(
     'default' => array(
-        'check_user_agent' => TRUE,
+        'check_user_agent' => FALSE,
         'allowed_user_agents' => array('Firefox','QHDA','XML-RPC for PHP 3.0.0.beta')
     )
 );
